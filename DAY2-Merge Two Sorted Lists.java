8b349bc4e56e19e2844c36448aab35d1920777e4
mahisrivastava1218 <98544175+mahisrivastava1218@users.noreply.github.com>
2022-10-01 11:06:07 +0530

    Update DAY2-Merge Two Sorted Lists.java

--- DAY2-Merge Two Sorted Lists.java
+++ DAY2-Merge Two Sorted Lists.java
@@ -8,33 +8,29 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+class Solution{
+    public ListNode mergeTwoLists(ListNode list1,ListNode list2){
         if(list1==null) return list2;
         if(list2==null) return list1;
-        if(list1==null && list2==null){
-            return null;
-        }
-    
-        ListNode temp=null;
         if(list1.val>list2.val){
-            temp=null;
+            ListNode temp=list1;
             list1=list2;
             list2=temp;
         }
         ListNode res=list1;
         while(list1!=null && list2!=null){
-            
+            ListNode temp=null;
             while(list1!=null && list1.val<=list2.val){
-                     
-                     temp=list1;
-                     list1=list1.next;
-             }
-         temp.next=list2;
-          temp=list1;
-          list1=list2;
-          list2=temp;
+                temp=list1;
+                list1=list1.next;
+            }
+            temp.next=list2;
+            temp=list1;
+            list1=list2;
+            list2=temp;
+            
         }
         return res;
+        
     }
 }
